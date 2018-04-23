c2225ca507e748e00c0793b07d325f81606bf5f1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsFormsApplication42/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication42 {
+    public partial class Form1 : Form {
+        Dictionary<string, string> ShortCaptions;
+
+        public Form1() {
+            InitializeComponent();
+            ShortCaptions = new Dictionary<string, string>();
+            ShortCaptions.Add("CustomerID", "ID");
+            ShortCaptions.Add("ContactName", "Name");
+            ShortCaptions.Add("ContactTitle", "Title");
+            ShortCaptions.Add("CompanyName", "Company");
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'nwindDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.nwindDataSet.Customers);
+        }
+
+        private void gridView1_CustomDrawColumnHeader(object sender, DevExpress.XtraGrid.Views.Grid.ColumnHeaderCustomDrawEventArgs e) {
+            if (e.Column == null) return;
+            SizeF s = e.Appearance.CalcTextSize(e.Cache, e.Info.Caption, e.Info.CaptionRect.Width);
+            if (s.Width >= e.Info.CaptionRect.Width) {
+                if (ShortCaptions.ContainsKey(e.Column.FieldName)) {
+                    e.Info.Caption = ShortCaptions[e.Column.FieldName];
+                }
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# XtraGrid - How to specify short captions for columns
+
+
+<p>This example demonstrates how to specify short captions for columns by handling GridView's CustomDrawColumnHeader event.</p>
+
+<br/>
+
+
